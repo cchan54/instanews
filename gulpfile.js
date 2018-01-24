f5869be7342b8be51bb46add0426926e741082e2
Cliff Chan <cchan17@live.ca>
2018-01-24 14:20:04 -0800

    add node,gulpfile,eslint,browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,39 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),           //Always first in load order
+ uglify = require('gulp-uglify'),
+ rename = require('gulp-rename'),
+ browserSync = require('browser-sync');
 
-gulp.task('default', defaultTask);
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+gulp.task('script', function() {
+ return gulp.src('./js/*.js')
+   .pipe(uglify())   //calls uglify to run
+   .pipe(rename({extname: '.min.js'})) //renames extension to ".min.js"
+   .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('css', function(){
+  return gulp.src('style.css')
+});
+
+gulp.task('watch', function() {
+ gulp.watch('./js/*.js', gulp.parallel('script'));
+});
+
+gulp.task('browser-sync', function() {
+ browserSync.init( {
+   server: {
+     baseDir: "./"
+   }
+ })
+})
+
+gulp.task('lint', function(){
+  
+
+});
+
+gulp.watch("./build/js/*.js").on('change', browserSync.reload);
+
+gulp.watch("style.css").on('change', browserSync.reload);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync')); //runs script in parallel
