51d3c64d6e1f496e307e494c608a4cbee93871ea
Cliff Chan <cchan17@live.ca>
2018-01-27 23:45:53 -0800

    mixins
    fixed mixins to make responsive

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
-// NYT API //
-
-// Built by LucyBot. www.lucybot.com
+  // Built by LucyBot. www.lucybot.com
 var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + $.param({
   'api-key': "d66269381eee43a2a61ef8c5e152affa"
 });
+
 $.ajax({
   url: url,
   method: 'GET',
   dataType: 'json'
 })
 
-.done(function(result) {
-  console.log(result);
-})
+.done(function(data){
+  var stories  = ''; 
+  var results = data.results.filter(function(value){
+    return value.multimedia.length >= 5;
+  })
+  // only want 12 stories
+  results.splice(12);
 
-.fail(function(err) {
-  throw err;
-});
+ $.each(results, function(key,value){
+   var images = value.multimedia[4].url,
+       words = value.abstract,
+       url = value.url;
 
-$
+       stories += '<li class="textpict">';
+       stories += '<a href=' + url + '>';
+       stories += '<div class="article" style="background-image: url(' + images + ')">';
+       stories += '<p class="words" >';
+       stories += words;
+       stories += '</p></div></a></li>';    
+      });
+    });
