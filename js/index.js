0587b0e8f6f746603263326cb1c17a3568019777
Cliff Chan <cchan17@live.ca>
2018-01-29 13:31:12 -0800

    api fix
    now working with title and img

--- js/index.js
+++ js/index.js
@@ -1,33 +1,49 @@
-  // Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "d66269381eee43a2a61ef8c5e152affa"
-});
+$(function() {
+  $("select").on("change", function() {
+    event.preventDefault();
 
-$.ajax({
-  url: url,
-  method: 'GET',
-  dataType: 'json'
-})
+    //load wheel
+    $(".loader").show();
 
-.done(function(data){
-  var stories  = ''; 
-  var results = data.results.filter(function(value){
-    return value.multimedia.length >= 5;
-  })
-  // only want 12 stories
-  results.splice(12);
+    //grabbing of stories
+    var selector = $(this).val();
+    var url = "https://api.nytimes.com/svc/topstories/v2/";
 
- $.each(results, function(key,value){
-   var images = value.multimedia[4].url,
-       words = value.abstract,
-       url = value.url;
+    url += selector + ".json";
+    url +=
+      "?" +
+      $.param({
+        "api-key": "d66269381eee43a2a61ef8c5e152affa"
+      });
 
-       stories += '<li class="textpict">';
-       stories += '<a href=' + url + '>';
-       stories += '<div class="article" style="background-image: url(' + images + ')">';
-       stories += '<p class="words" >';
-       stories += words;
-       stories += '</p></div></a></li>';    
+    $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json"
+    })
+    .done(function(data) {
+      var stories = "";
+
+
+      var results = data.results.filter(function(item){
+        return item.multimedia.length;
+      }).slice(0, 12);
+
+      console.log(results);
+
+      $.each(results, function(key, value) {
+        var image = value.multimedia[4].url,
+            words = value.abstract,
+            url = value.url;
+
+          stories += '<div class="article">';
+          stories += '<a href="' + url + '">' + words + '</a>';
+          stories += '<img class="article-img" src="' + image + '">';
+          stories += '</div>';
+
+        $('.gallery').append(stories);
+
       });
     });
+  });
+});
