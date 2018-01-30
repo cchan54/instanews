897f622ffd03cdfc77ff6595dcb8498f4d87215f
Cliff Chan <cchan17@live.ca>
2018-01-29 19:40:19 -0800

    added loading gif
    made it so it hides after an item is clicked

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-$(function() {
-  $("select").on("change", function() {
+$(document).ready(function() {
+  $('.loading').hide();
+  $("select").on("change", function(event) {
     event.preventDefault();
 
-    //load wheel
-    $(".loader").show();
+  $('.loading').show();
 
     //grabbing of stories
     var selector = $(this).val();
     var url = "https://api.nytimes.com/svc/topstories/v2/";
-
     url += selector + ".json";
     url +=
       "?" +
@@ -16,35 +15,45 @@ $(function() {
         "api-key": "d66269381eee43a2a61ef8c5e152affa"
       });
 
+    $('.gallery').empty(),
+    
     $.ajax({
       url: url,
       method: "GET",
       dataType: "json"
     })
     .done(function(data) {
-      var stories = "";
-      var results = data.results.filter(function(item){
-        return item.multimedia.length >=5;
+      
+      var results = data.results.filter(function(el){
+        return el.multimedia.length >0;
       }).slice(0, 12);
 
       console.log(results);
 
-        $.each(results, function(key, value) {
-        var image = value.multimedia[4].url,
-            headline = value.abstract,
-            url = value.url;
-          
-          stories += '<li>';
-          // stories += '<a href="' + url + '">' + headline + '</a>';
+    $.each(results, function(index, value) {
+      var image = value.multimedia[4].url,
+          headline = value.abstract,
+          link = value.url;
+        
+        var stories = "";
           stories += '<div class="article" style="background-image: url(' + image + ')">';
-          stories += '<div class="caption">'
-          stories += '<p>'+ headline +'</p>';
-          stories += '</div>'
+          stories += '<div class="caption">';
+          stories += '<p><a href="' + link + '">' + headline + '</a></p>';
           stories += '</div>';
-          stories += '</li>';
+          stories += '</div>';
 
+
+
+        $('.loading').hide();
         $('.gallery').append(stories);
+
+
+
     });
+
+      }).fail(function (err) {
+      throw err;
   });
 });
 });
+
